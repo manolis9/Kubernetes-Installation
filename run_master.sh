bcd1351f0b1d7210cfdb3455df4ce7c0d37fffde
Harsha <devkhv129@gmail.com>
2019-01-14 13:10:37 -0800

    Update run_master.sh

--- run_master.sh
+++ run_master.sh
@@ -2,8 +2,8 @@ sudo apt-get update
 sudo apt-get upgrade -y
 sudo rm -rf /etc/kubernetes
 sudo apt-get install -y apt-transport-https curl
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
-cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
+suod curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+sudo cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
 deb https://apt.kubernetes.io/ kubernetes-xenial main
 EOF
 sudo apt-get install -y docker.io kubelet kubeadm kubectl kubernetes-cni nfs-common
