846298ceb79b2f323343f9139494158c7c804473
Harsha <devkhv129@gmail.com>
2019-01-14 13:14:16 -0800

    Update run_master.sh

--- run_master.sh
+++ run_master.sh
@@ -6,6 +6,7 @@ sudo curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add
 sudo cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
 deb https://apt.kubernetes.io/ kubernetes-xenial main
 EOF
+apt-get update
 sudo apt-get install -y docker.io kubelet kubeadm kubectl kubernetes-cni nfs-common
 sudo systemctl start docker
 sudo systemctl enable docker
